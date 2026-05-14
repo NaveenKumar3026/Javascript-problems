75575fd57210c6bfc844ccbc162b06a7b024a45d
Naveen Kumar <naveen3026mit@gmail.com>
2026-05-14 21:42:20 +0530

    Add files via upload

---
+++ example4.js
@@ -0,0 +1,12 @@
+var fActor= "Vijay Devarakonda"
+var fPlayer= "Virat Kohli"
+var fMovie= "Geetha Govindam"
+
+function favourite()
+{
+    console.log("Favourite Actor: "+fActor)
+    console.log("Favaourite Player: "+fPlayer)
+    console.log("Favourite Movie: "+fMovie)
+}
+
+favourite()
