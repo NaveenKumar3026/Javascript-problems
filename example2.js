0da30b79ba20831d9de694f8f82067b001b94853
Naveen Kumar <naveen3026mit@gmail.com>
2026-05-14 21:41:37 +0530

    Add files via upload

---
+++ example2.js
@@ -0,0 +1,24 @@
+function samsung()
+{
+    console.log("Samsung")
+}
+
+function vivo()
+{
+    console.log("Vivo")
+}
+
+function iphone()
+{
+    console.log("Iphone")
+}
+
+function oppo()
+{
+    console.log("Oppo")
+}
+
+samsung();
+vivo();
+iphone();
+oppo();
